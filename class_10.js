8dac5c9dc999c31c44548c777a51fdf873da5f1b
Freddy910829 <freddygoberna@gmail.com>
2022-10-24 17:44:13 -0600

    Adding gitignore and first files of the project

---
+++ class_10.js
@@ -0,0 +1,12 @@
+// Seleccionando elementos por su etiqueta
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const input = document.querySelector("input")
+
+// Seleccionadno elementos por su clase 
+const parrafito = document.querySelector(".parrafito");
+
+// Seleccionando elementos por su ID
+const pid = document.querySelector("#pid");
+
+console.log(input);
